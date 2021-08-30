37ab711c9e325b08d46d79a2dd731a49ee4bb82c
James Murray <james.w.murray@student.uts.edu.au>
2021-08-30 21:48:23 +1000

    Updated plots and explanations

--- linear_regression_basics.R
+++ linear_regression_basics.R
@@ -5,8 +5,12 @@ library(tidymodels)
 library(openxlsx)
 
 # Load Data 
+# set working directory to the base directory where both the data and the .r file are for ease
+# setwd(" ")
+# Read in Data from spreadsheet and convert it to a tibble to make it 'tidy' and easy to read and use
+sals <- read.xlsx("wk3-2.xlsx") %>% as_tibble()
 
-sals <- read.xlsx("/Users/james/Downloads/wk3-2.xlsx")
+# Check Data 
 sals
 
 # Scatter Plot with differences in wages over time between male and female
@@ -17,8 +21,9 @@ sals %>%
 
 # Create an interaction variable
  sals_1 <- sals %>% mutate(
-   experience_male = Experience*Male
- ) %>% tibble()
+   experience_male = Experience*Male, # create a binary variable that accounts for both teh differences in sex and experience -- this was apparent in the EDA above
+   Male = as_factor(Male) #convert to a factor for ease in future 
+ )
 
  sals_1
  
@@ -35,17 +40,22 @@ sals %>%
  lm_fit <- lm_mod %>% 
    fit(Salary ~ Experience + Male + experience_male, data = sals_1)
  
- lm_fit %>% summary()
- lm_fit$fit %>% glance()
- lm_fit$fit %>% summary() %>% tidy() 
+ lm_fit %>% tidy() # the individual variable performance
+ lm_fit %>% glance()  # The summary stats for the overall model
+ 
 ### Create Plots for Checking the assumptions (residuals) ====
 
 # Add residual info to the DF 
-sals_2 <- lm_fit$fit %>% augment(sals_1)
+sals_2 <- lm_fit$fit %>% augment(sals_1) #pull the fit diagnostics for each observation to enable them to be plotted
 sals_2 
+lm_fit$fit 
 # Plot residuals  
 sals_2 %>% ggplot(aes(.fitted)) +
    geom_point(aes(y = .resid), colour = "blue")
+
+sals_2 %>% ggplot(aes(Salary), fill = Male) +
+  geom_point(aes(y = .fitted))
+
 # Cooks Distance 
 sals_2 %>% ggplot(aes(Obs)) +
   geom_col(aes(y = .cooksd)) +
@@ -64,6 +74,7 @@ sals_2 %>% ggplot(aes(Experience)) +
   geom_hline(aes(yintercept = 0))
 
 ### Create a model with a log^10 y variable ====
+
 # create a log10 salary variable
 sals_3 <- sals_1 %>% mutate(
   log_sal = log10(Salary), 
@@ -73,14 +84,20 @@ sals_3
 
 ### Build model ====
 
+# use the engineered features including the 'log' of the target variable
+
 lm_fit_1 <- lm_mod %>% 
   fit(log_sal ~ Experience + Male + experience_male + exp_2, data = sals_3)
 
 ### Check the model ====
+
 # overall model performance R2 etc
-lm_fit_1$fit %>% glance() %>% tidy()
-results_1 <- lm_fit_1$fit %>% tidy()
+lm_fit_1 %>% glance() %>% tidy() # each observation performance
+lm_fit_1 %>% glance() # overall performance of the model - note the R2 and we haven't removed a single outlier...
+lm_fit_1$fit %>% summary() #note the male variable and how it is now much more significant 
+results_1 <- lm_fit_1 %>% tidy() # store the results for later usage 
 results_1
+
 ### interpret results ====
 # backwards convert log10 for interpretations and interpret results as percentage increases per unit of increase not inclusive of the intercept. 
 
@@ -93,17 +110,23 @@ results_1 %>% select(term, estimate) %>% pivot_wider(names_from = term, values_f
 )
 
 #Inspect plots of the results 
-par(mfrow = c(2,2))
-lm_fit_1$fit%>% plot()
+par(mfrow = c(2,2)) #split the plotting area into 2 x 2 to make it easy to plot the basic plots
+lm_fit_1$fit%>% plot() #plot the basic plots in the
 
 lm_fit_1 
  # Percentage scales are a multiplicative relationship by using a log transformation it turns it into an additive relationship.  
-sals_3 <- sals_3 %>% mutate(
-  Male = as_factor(Male)
-)
-res_1 <- tidy(lm_fit_1)
-res_1
 
+# Plot the fitted model predictions for both males and females with the features engineered
+# Features altered for this model were
+# - Log10 for the target variable (Salaries) to account for the leveling off as experience continues to increase // salaries can't go up forever in a linear fashion, they must plateau at some point in time (around 13 yrs for females and 17 yrs for males in our model)
+# - experienced squared to account for the 'inverted U' found in the residuals in previous models
+# - experience variable to account for both the male and female differences identified in the original and simple EDA
+
+# Plot the predicted model for both males and females -- note this was done using only a linear model with 'careful' feature engineering... A simple model that is easily explainable and understood by all that appears to perform well. 
+
+# add the model predictions to the results for plotting with a confidence interval (.95)
 sals_3 <- augment(lm_fit_1, sals_3, interval = "confidence")
+sals_3
+# Plot the model predictions
 sals_3 %>% ggplot(aes(x = Experience, y = .pred), colour = Male) +
   geom_point(aes(colour = Male)) 
